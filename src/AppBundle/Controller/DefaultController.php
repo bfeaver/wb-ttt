7aeb17bde0e35b9e5f596b7ad835d410351afbaf
Brian Feaver <brian.feaver@gmail.com>
2015-01-17 11:26:42 -0800

    Add a simple functional test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,21 +4,20 @@ namespace AppBundle\Controller;
 
 use AppBundle\Form\NestedGridType;
 use AppBundle\Game\GridFactory;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Config\Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
         $grid = $this->getGridFactory()->createNestedGrid(2);
 
         $form = $this->createForm(new NestedGridType(), $grid);
-
         $form->handleRequest($request);
 
         if ($form->isValid() && false !== $winner = $grid->getWinner()) {
--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,12 +8,36 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $this->markTestSkipped('came with framework, remove...');
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/app/example');
+        $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Crazy Tic-Tac-Toe")')->count() > 0);
+    }
+
+    public function testXIsWinner()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $buttonCrawlerNode = $crawler->selectButton('Submit');
+        $form = $buttonCrawlerNode->form();
+        $formResponseCrawler = $client->submit($form, [
+            'nested[squares][0][squares][0][value]' => 'X',
+            'nested[squares][0][squares][1][value]' => 'X',
+            'nested[squares][0][squares][2][value]' => 'X',
+
+            'nested[squares][1][squares][0][value]' => 'X',
+            'nested[squares][1][squares][4][value]' => 'X',
+            'nested[squares][1][squares][8][value]' => 'X',
+
+            'nested[squares][2][squares][2][value]' => 'X',
+            'nested[squares][2][squares][5][value]' => 'X',
+            'nested[squares][2][squares][8][value]' => 'X',
+        ]);
+
+        $this->assertTrue($formResponseCrawler->filter('html:contains("The winner is X")')->count() > 0);
     }
 }
