9421be4445f769c8a218545bbbe8c93d5c50055d
Brian Feaver <brian.feaver@gmail.com>
2015-01-16 20:04:21 -0800

    Change invalid value to InvalidArgException

--- src/AppBundle/Game/XoSquare.php
+++ src/AppBundle/Game/XoSquare.php
@@ -32,7 +32,7 @@ class XoSquare implements Square
     public function setValue($value)
     {
         if ($value !== 'X' && $value !== 'O' && !is_null($value)) {
-            throw new \RuntimeException(sprintf('Value must be X or O, value %s given.', $value));
+            throw new \InvalidArgumentException(sprintf('Value must be X or O, value %s given.', $value));
         }
         $this->value = $value;
     }
