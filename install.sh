e01e0367018e6d2cde9469fd71e6bcd0d7d7267e
Brian Feaver <brian.feaver@gmail.com>
2015-01-17 10:07:01 -0800

    Load the vagrant box without any manual steps
    Also ignores the parameters.yml file like the project should and builds
    it from the dist file.

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 apt-get update
-apt-get install apache2
-apt-get install php5 php5-xdebug
-apt-get install mysql-server mysql-client
+apt-get install -y apache2 php5 php5-xdebug php5-mysql mysql-server mysql-client
+
+cp /vagrant/files/apache2.conf /etc/apache2/apache2.conf
+cp /vagrant/files/envvars /etc/apache2/envvars
+cp /vagrant/files/default.conf /etc/apache2/sites-available/000-default.conf
+cp /vagrant/files/xdebug.ini /etc/php5/mods-available/xdebug.ini
+
+# Reload configuration
+service apache2 restart
+
+# Had some issues if this didn't exist. Could turn off DB support
+# but was easier to just add this to get it working.
+mysql -uroot -proot -e 'CREATE DATABASE IF NOT EXISTS symfony;'
